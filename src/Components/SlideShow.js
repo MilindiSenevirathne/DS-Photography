4644f77e5367a677018de658fe0a72df59f8c9bb
MilindiSenevirathne <milindianu@gmail.com>
2022-08-28 01:28:00 +0530

    about me added

--- src/Components/SlideShow.js
+++ src/Components/SlideShow.js
@@ -1,44 +1,64 @@
-import './Slideshow.css';
-import { useState, useEffect } from 'react';
-import Image1 from '../Assets/5.jpg'
-import Image2 from '../Assets/behance-3.jpg';
-import Image3 from '../Assets/m2.jpg';
-import Image4 from '../Assets/me4.jpg'
-import Image5 from '../Assets/me5.jpg'
+import "./Slideshow.css";
+import { useState, useEffect } from "react";
+import Image1 from "../Assets/5.jpg";
+import Image2 from "../Assets/behance-3.jpg";
+import Image3 from "../Assets/m2.jpg";
+import Image4 from "../Assets/me4.jpg";
+import Image5 from "../Assets/me5.jpg";
 
 const colors = [Image1, "#00C49F", "#FFBB28", "red", "pink", "black"];
 const images = [
-    { id: 1, src: Image1, title: 'foo', description: 'bar' },
-    { id: 2, src: Image2, title: 'foo', description: 'bar' },
-    { id: 3, src: Image3, title: 'foo', description: 'bar' },
-    { id: 4, src: Image4, title: 'foo', description: 'bar' },
-    { id: 5, src: Image5, title: 'foo', description: 'bar' },
-  ];
+  { id: 1, src: Image1, title: "foo", description: "bar" },
+  { id: 2, src: Image2, title: "foo", description: "bar" },
+  { id: 3, src: Image3, title: "foo", description: "bar" },
+  { id: 4, src: Image4, title: "foo", description: "bar" },
+  { id: 5, src: Image5, title: "foo", description: "bar" },
+];
 const delay = 5000;
 
-const Slideshow =() =>{
+const Slideshow = () => {
+  const [index, setIndex] = useState(0);
 
-    const [index, setIndex] = useState(0);
+  useEffect(() => {
+    setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === images.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
 
-    useEffect(() => {
-        setTimeout(
-          () =>
-            setIndex((prevIndex) =>
-              prevIndex === images.length - 1 ? 0 : prevIndex + 1
-            ),
-          delay
-        );
-    
-        return () => {};
-      }, [index]);
+    return () => {};
+  }, [index]);
 
   return (
     <div className="mt-4 slideshow">
-      <div className="slideshowSlider" style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
-      { images.map(({id, src, title, description}) => <img key={id} src={src} title={title} alt={description} height="600px"/>)}
+      <div
+        className="slideshowSlider"
+        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+      >
+        {images.map(({ id, src, title, description }) => (
+          <img
+            className="me-4 slide_image"
+            key={id}
+            src={src}
+            title={title}
+            alt={description}
+            height="600px"
+            style={id ? {transition:"ease-in-out"}:{}}
+          />
+        ))}
       </div>
+      {/* <marquee
+        behavior="scroll"
+        direction="left"
+        scrolldelay="1000"
+        scrollamount="100"
+      >
+       { images.map(({id, src, title, description}) => <img key={id} src={src} title={title} alt={description} height="600px"/>)}
+      </marquee> */}
     </div>
   );
-}
+};
 
 export default Slideshow;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,52 +1,111 @@
 import Slideshow from "../Components/SlideShow";
+import "./Home.css";
+import Image from "../Assets/camera.png";
+import Facebook from "../Assets/facebook.png";
+import Instagram from "../Assets/instagram (1).png";
+import Image2 from "../Assets/me4.jpg";
 
 const Home = () => {
   return (
     <div>
       <div>
-      <div className="d-flex justify-content-around navbar" style={{}}>
-        <div className="nav_links">
-          <ul class="nav flex-column" style={{color:"black"}}>
-            <li class="nav-item" >
-              <a class="nav-link active" href="#">
-                WELCOME
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                ABOUT ME
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                MY WORKS
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                PORTFOLIO
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                CONTACT
-              </a>
-            </li>
-          </ul>
-        </div>
+        <div
+          className="d-flex justify-content-around navbar"
+          style={{ boxShadow: "none" }}
+        >
+          <div className="nav_links">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a
+                  class="nav-link active"
+                  href="#"
+                  style={{ color: "#bfbdbd" }}
+                >
+                  WELCOME
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" style={{ color: "#bfbdbd" }}>
+                  ABOUT ME
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" style={{ color: "#bfbdbd" }}>
+                  MY WORKS
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" style={{ color: "#bfbdbd" }}>
+                  PORTFOLIO
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" style={{ color: "#bfbdbd" }}>
+                  CONTACT
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div
+            class="d-flex justify-content-center line"
+            style={{ height: "150px" }}
+          >
+            <div class="vr"></div>
+          </div>
 
-        <div className="name">
-            <h1 style={{fontFamily:"Space Mono"}}>DS Photography</h1>
-        </div>
+          <div className="name_div">
+            <img src={Image} alt="Snow" className="camera_img" />
+            <h1 className="name">DS Photography</h1>
+          </div>
 
-        <div>
-            <h6>Best phogragraphs and <br/>give value for money.</h6>
+          <div
+            class="d-flex justify-content-center line"
+            style={{ height: "150px" }}
+          >
+            <div class="vr"></div>
+          </div>
+
+          <div>
+            <div className="desc1">
+              Best phogragraphs and <br />
+              give value for money.
+            </div>
+            <div className="icons">
+                <a href="facebook.com"><img src={Facebook} className="me-2 fb"/></a>
+                <a href="facebook.com"><img src={Instagram} className="insta"/></a>
+            </div>
+          </div>
         </div>
-      </div>
       </div>
 
       <div>
         <Slideshow />
+      </div>
+
+      <div className="location">
+        <h5>Hirana. Panadura</h5>
+        <div class="d-flex justify-content-center line">
+          <div class="vr"></div>
+        </div>
+        <div>
+          <h6 className="mt-2">
+            We make your special days with splendid memories to last a lifetime.
+          </h6>
+        </div>
+      </div>
+
+      <div className="mt-4">
+        <h1 className="about">About Me</h1>
+        <h6 className="about_name">Disal Senevirathne</h6>
+        <div>
+        <img src={Image2} alt="Snow" className="about_img" />
+        <div className="box_1">
+            <div className="box_2">
+                <h4>hvshjsvchscvskcjsc c nsc nmsc jscjhsvchkasvk.csc</h4>
+            </div>
+        </div>
+        <img src={Image2} alt="Snow" className="about_img" />
+        </div>
       </div>
     </div>
   );
