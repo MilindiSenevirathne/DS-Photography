ee204955b25a7e61e7c32675d26ba024f0368d22
MilindiSenevirathne <milindianu@gmail.com>
2022-08-28 23:38:32 +0530

    changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,7 +4,8 @@ import Image from "../Assets/camera.png";
 import Facebook from "../Assets/facebook.png";
 import Instagram from "../Assets/instagram (1).png";
 import Image2 from "../Assets/Disal.jpg";
-import Image3 from "../Assets/2.jpg";
+import Wedding from "../Assets/Home/H3.jpg"
+import Wedding2 from "../Assets/Home/H6.jpg"
 
 const Home = () => {
   return (
@@ -102,18 +103,18 @@ const Home = () => {
       <div className="mt-4 my_photography">
         <div className="mt-4 box">
           <div className="d-flex justify-content-center box_2">
-          <div className="box_3">
-            <h3 className="about_name2">My Photography</h3>
-            <h4 className="desc_2">
-              I am a Texas based wedding and portrait photographer. I started
-              photography like most do- as a hobby! I would have never dreamt
-              that it would eventually turn into a career for me. After
-              beginning as a hobby I realized that I liked taking photos and
-              making people FEEL a certain way more than I thought possible! I
-              dived into practicing more, educating myself and thus started the
-              beginning of a wonderful thing that is now my full-time job.
-            </h4>
-          </div>
+            <div className="box_3">
+              <h3 className="about_name2">My Photography</h3>
+              <h4 className="desc_2">
+                I am a Texas based wedding and portrait photographer. I started
+                photography like most do- as a hobby! I would have never dreamt
+                that it would eventually turn into a career for me. After
+                beginning as a hobby I realized that I liked taking photos and
+                making people FEEL a certain way more than I thought possible! I
+                dived into practicing more, educating myself and thus started
+                the beginning of a wonderful thing that is now my full-time job.
+              </h4>
+            </div>
           </div>
         </div>
       </div>
@@ -137,6 +138,29 @@ const Home = () => {
           </div>
           <div className="">
             <img src={Image2} alt="Snow" className="about_img" />
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-4">
+        <h1 className="about">My Works</h1>
+        <h6 className="about_name">Work Types</h6>
+        <div className="">
+          <div>
+            <h4 className="desc_2" style={{textAlign:"center"}}>
+              PassionArt wedding photography sri lanka seek to portray all
+              brides & bridegrooms in their own natural style<br></br> and we
+              make the photos to be expressions of their natural charm. Blending
+              that style with our creativeness<br></br> we strive to make their
+              wedding day photos to be really awesome.
+            </h4>
+          </div>
+          <div>
+          <div className="work_div">
+            <img src={Wedding} alt="Snow" className="work_img"/>
+            <img src={Wedding2} alt="Snow" className="hidden_img"/>
+            <h1 className="work_type">Weddings</h1>
+          </div>
           </div>
         </div>
       </div>
