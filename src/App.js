f54d6bcd97c6abcaefaf467f95b3578d0ce62555
MilindiSenevirathne <milindianu@gmail.com>
2022-09-01 21:50:36 +0530

    image gallery added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
 import './App.css';
 import Home from './Pages/Home';
+import { BrowserRouter as Router, Switch, Route, Link, BrowserRouter } from 'react-router-dom';
+import Weddings from './Pages/Weddings';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+   {/* <BrowserRouter>
+  
+   <Route exact path='*' element={<Home />}/>
+        <Route path='/weddings' element={<Weddings/>}/>
+   
+    </BrowserRouter> */}
+    {/* <Home /> */}
+    <Weddings />
     </div>
   );
 }
