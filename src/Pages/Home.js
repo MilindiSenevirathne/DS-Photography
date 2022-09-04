2e19129c4784dd849bb406c66018b5f03f3b7199
MilindiSenevirathne <milindianu@gmail.com>
2022-09-04 13:37:26 +0530

    changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -41,7 +41,7 @@ import SimpleImageSlider from "react-simple-image-slider";
 import C1 from "../Assets/3.jpg";
 import C2 from "../Assets/4.jpg";
 import C3 from "../Assets/5.jpg";
-import {Map, GoogleApiWrapper} from 'google-maps-react';
+import { Map, GoogleApiWrapper } from "google-maps-react";
 import Header from "../Components/Header";
 
 const Home = () => {
@@ -236,32 +236,54 @@ const Home = () => {
 
   return (
     <div>
-     <header>
+      <header>
         <Header />
-     </header>
+      </header>
 
       <div className="content">
-      <div className="slides">
-        <Slideshow />
-      </div>
-
-      <div className="location">
-        <h5>Hirana. Panadura</h5>
-        <div class="d-flex justify-content-center line">
-          <div class="vr"></div>
+        <div className="slides">
+          <Slideshow />
         </div>
-        <div>
-          <h6 className="mt-2">
-            We make your special days with splendid memories to last a lifetime.
-          </h6>
-        </div>
-      </div>
 
-      <div className="mt-4 my_photography" id="welcome">
-        <div className="mt-4 box">
-          <div className="d-flex justify-content-center box_2">
-            <div className="box_3">
-              <h3 className="about_name2">My Photography</h3>
+        <div className="location">
+          <h5>Hirana. Panadura</h5>
+          <div class="d-flex justify-content-center line">
+            <div class="vr"></div>
+          </div>
+          <div>
+            <h6 className="mt-2">
+              We make your special days with splendid memories to last a
+              lifetime.
+            </h6>
+          </div>
+        </div>
+
+        <div className="mt-4 my_photography" id="welcome">
+          <div className="mt-4 box">
+            <div className="d-flex justify-content-center box_2">
+              <div className="box_3">
+                <h3 className="about_name2">My Photography</h3>
+                <h4 className="desc_2">
+                  I am a Texas based wedding and portrait photographer. I
+                  started photography like most do- as a hobby! I would have
+                  never dreamt that it would eventually turn into a career for
+                  me. After beginning as a hobby I realized that I liked taking
+                  photos and making people FEEL a certain way more than I
+                  thought possible! I dived into practicing more, educating
+                  myself and thus started the beginning of a wonderful thing
+                  that is now my full-time job.
+                </h4>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-4" id="about">
+          <h1 className="about">About Me</h1>
+          <h6 className="about_name">Disal Senevirathne</h6>
+          <div className="d-flex justify-content-center">
+            <div className="box_1">
+              <h3 className="about_name2">I'm Disal Senevirathne</h3>
               <h4 className="desc_2">
                 I am a Texas based wedding and portrait photographer. I started
                 photography like most do- as a hobby! I would have never dreamt
@@ -271,57 +293,37 @@ const Home = () => {
                 dived into practicing more, educating myself and thus started
                 the beginning of a wonderful thing that is now my full-time job.
               </h4>
+              <button className="about_button">Learn More</button>
+            </div>
+            <div className="">
+              <img src={Image2} alt="Snow" className="about_img" />
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="mt-4" id="about">
-        <h1 className="about">About Me</h1>
-        <h6 className="about_name">Disal Senevirathne</h6>
-        <div className="d-flex justify-content-center">
-          <div className="box_1">
-            <h3 className="about_name2">I'm Disal Senevirathne</h3>
-            <h4 className="desc_2">
-              I am a Texas based wedding and portrait photographer. I started
-              photography like most do- as a hobby! I would have never dreamt
-              that it would eventually turn into a career for me. After
-              beginning as a hobby I realized that I liked taking photos and
-              making people FEEL a certain way more than I thought possible! I
-              dived into practicing more, educating myself and thus started the
-              beginning of a wonderful thing that is now my full-time job.
-            </h4>
-            <button className="about_button">Learn More</button>
-          </div>
+        <div className="mt-4" id="works">
+          <h1 className="about">My Works</h1>
+          <h6 className="about_name">Work Types</h6>
           <div className="">
-            <img src={Image2} alt="Snow" className="about_img" />
-          </div>
-        </div>
-      </div>
-
-      <div className="mt-4" id="works">
-        <h1 className="about">My Works</h1>
-        <h6 className="about_name">Work Types</h6>
-        <div className="">
-          <div>
-            <h4 className="desc_2" style={{ textAlign: "center" }}>
-              PassionArt wedding photography sri lanka seek to portray all
-              brides & bridegrooms in their own natural style<br></br> and we
-              make the photos to be expressions of their natural charm. Blending
-              that style with our creativeness<br></br> we strive to make their
-              wedding day photos to be really awesome.
-            </h4>
-          </div>
-          <div className="works">
-            <div className="work_div">
-              <button className="btn btn-light btn-rounded work_button">
-                View Albums
-              </button>
-              <button className="btn btn-warning btn-rounded work_type">
-                Weddings
-              </button>
+            <div>
+              <h4 className="desc_2" style={{ textAlign: "center" }}>
+                PassionArt wedding photography sri lanka seek to portray all
+                brides & bridegrooms in their own natural style<br></br> and we
+                make the photos to be expressions of their natural charm.
+                Blending that style with our creativeness<br></br> we strive to
+                make their wedding day photos to be really awesome.
+              </h4>
             </div>
-            {/* <div className="me-2 work_div2">
+            <div className="works">
+              <div className="work_div">
+                <button className="btn btn-light btn-rounded work_button">
+                  View Albums
+                </button>
+                <button className="btn btn-warning btn-rounded work_type">
+                  Weddings
+                </button>
+              </div>
+              {/* <div className="me-2 work_div2">
               <button className="btn btn-light btn-rounded work_button">
                 View Albums
               </button>
@@ -329,92 +331,100 @@ const Home = () => {
                 Homecomings
               </button>
             </div> */}
-            <div className="work_div3">
-              <button className="btn btn-light btn-rounded work_button">
-                View Albums
-              </button>
-              <button className="btn btn-warning btn-rounded work_type">
-                Pre Shoots
-              </button>
-            </div>
-            <div className="work_div4">
-              <button className="btn btn-light btn-rounded work_button">
-                View Albums
-              </button>
-              <button className="btn btn-warning btn-rounded work_type">
-                Engagements
-              </button>
-            </div>
-            <div className="work_div5">
-              <button className="btn btn-light btn-rounded work_button">
-                View Albums
-              </button>
-              <button className="btn btn-warning btn-rounded work_type">
-                Birthday
-              </button>
+              <div className="work_div3">
+                <button className="btn btn-light btn-rounded work_button">
+                  View Albums
+                </button>
+                <button className="btn btn-warning btn-rounded work_type">
+                  Pre Shoots
+                </button>
+              </div>
+              <div className="work_div4">
+                <button className="btn btn-light btn-rounded work_button">
+                  View Albums
+                </button>
+                <button className="btn btn-warning btn-rounded work_type">
+                  Engagements
+                </button>
+              </div>
+              <div className="work_div5">
+                <button className="btn btn-light btn-rounded work_button">
+                  View Albums
+                </button>
+                <button className="btn btn-warning btn-rounded work_type">
+                  Birthday
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="mt-4" id="captures">
-        <h1 className="about">Favorite Captures</h1>
-        <h6 className="about_name">BEST-LOVE</h6>
-        <div>
+        <div className="mt-4" id="captures">
+          <h1 className="about">Favorite Captures</h1>
+          <h6 className="about_name">BEST-LOVE</h6>
           <div>
-            <h4 className="desc_2" style={{ textAlign: "center" }}>
-              Passing wondrous moments may not come with the same glow again.
-              But we freeze them in beautiful <br></br> photographs to be viewed
-              and reviewed to ones heart’s content. And that is the work we love{" "}
-              <br></br> most – just taking pretty pictures.
-            </h4>
+            <div>
+              <h4 className="desc_2" style={{ textAlign: "center" }}>
+                Passing wondrous moments may not come with the same glow again.
+                But we freeze them in beautiful <br></br> photographs to be
+                viewed and reviewed to ones heart’s content. And that is the
+                work we love <br></br> most – just taking pretty pictures.
+              </h4>
+            </div>
+            <div className="mt-4">
+              <Filter
+                popular={popular}
+                setFiltered={setFiltered}
+                activeGenre={activeGenre}
+                setActiveGenre={setActiveGenre}
+              />
+              <div className="mt-4"></div>
+              <motion.div layout className="popular-images">
+                <AnimatePresence>
+                  {filtered.map((image) => {
+                    return <ImageComponent key={image.id} image={image} />;
+                  })}
+                </AnimatePresence>
+              </motion.div>
+            </div>
           </div>
-          <div className="mt-4">
-            <Filter
-              popular={popular}
-              setFiltered={setFiltered}
-              activeGenre={activeGenre}
-              setActiveGenre={setActiveGenre}
+        </div>
+
+        <div className="mt-4" id="client">
+          <h1 className="about">Clients' Experences</h1>
+          <h6 className="about_name">Testimonial</h6>
+          <div>
+            <div>
+              <h4 className="desc_2" style={{ textAlign: "center" }}>
+                Passing wondrous moments may not come with the same glow again.
+                But we freeze them in beautiful <br></br> photographs to be
+                viewed and reviewed to ones heart’s content. And that is the
+                work we love <br></br> most – just taking pretty pictures.
+              </h4>
+            </div>
+          </div>
+          <div className="d-flex justify-content-center slider">
+            <SimpleImageSlider
+              width={896}
+              height={504}
+              images={image}
+              showBullets={true}
+              showNavs={true}
             />
-            <div className="mt-4"></div>
-            <motion.div layout className="popular-images">
-              <AnimatePresence>
-                {filtered.map((image) => {
-                  return <ImageComponent key={image.id} image={image} />;
-                })}
-              </AnimatePresence>
-            </motion.div>
+          </div>
+
+          <div className="d-flex justify-content-center mt-4">
+            <iframe
+              style={{ width: "35%", height: "400px", marginTop: "50px" }}
+              id="gmap_canvas"
+              src="https://maps.google.com/maps?q=disal%20senevirathne%20&t=&z=17&ie=UTF8&iwloc=&output=embed"
+              frameborder="0"
+              scrolling="no"
+              marginheight="0"
+              marginwidth="0"
+            ></iframe>
           </div>
         </div>
-      </div>
-
-      <div className="mt-4" id="client">
-        <h1 className="about">Clients' Experences</h1>
-        <h6 className="about_name">Testimonial</h6>
-        <div>
-          <div>
-            <h4 className="desc_2" style={{ textAlign: "center" }}>
-              Passing wondrous moments may not come with the same glow again.
-              But we freeze them in beautiful <br></br> photographs to be viewed
-              and reviewed to ones heart’s content. And that is the work we love{" "}
-              <br></br> most – just taking pretty pictures.
-            </h4>
-          </div>
-        </div>
-        <div className="d-flex justify-content-center slider">
-          <SimpleImageSlider
-            width={896}
-            height={504}
-            images={image}
-            showBullets={true}
-            showNavs={true}
-          />
-        </div>
-
-        <div className="d-flex justify-content-center mt-4">
-        <iframe style={{width:"35%", height:"400px", marginTop:"50px"}}  id="gmap_canvas" src="https://maps.google.com/maps?q=disal%20senevirathne%20&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-        </div>
-      </div>
       </div>
     </div>
   );
