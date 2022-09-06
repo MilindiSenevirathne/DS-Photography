c5c1de0c1adb35d06325a78f683593b495d0802d
vihan pamudya <vihanpamudya12@gmail.com>
2022-09-07 00:45:58 +0530

    footer added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -220,7 +220,7 @@ const Home = () => {
         title: "foo",
         description: "bar",
         genre_ids: [3, 25, 120],
-      }
+      },
     ];
 
     setPopular(favorite);
@@ -503,9 +503,159 @@ const Home = () => {
               </div>
             </div>
           </div>
-
         </div>
       </div>
+
+      <footer
+        class="text-center text-lg-start text-white mt-5"
+        style={{ backgroundColor: "#fcb323" }}
+      >
+        <section class="d-flex justify-content-lg-between"></section>
+        <section class="">
+          <div class="container text-center text-md-start mt-5">
+            <div class="row mt-3">
+              <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                <h6 class="text-uppercase fw-bold mb-4">
+                  <i class="fas fa-gem me-2"></i>Company name
+                </h6>
+                <p>
+                  Here you can use rows and columns to organize your footer
+                  content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                  elit.
+                </p>
+              </div>
+
+              <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 class="text-uppercase fw-bold mb-4">Products</h6>
+                <p>
+                  <a href="#!" class="text-reset">
+                    Angular
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" class="text-reset">
+                    React
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" class="text-reset">
+                    Vue
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" class="text-reset">
+                    Laravel
+                  </a>
+                </p>
+              </div>
+
+              <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 class="text-uppercase fw-bold mb-4">Useful links</h6>
+                <p>
+                  <a href="#!" class="text-reset">
+                    Pricing
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" class="text-reset">
+                    Settings
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" class="text-reset">
+                    Orders
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" class="text-reset">
+                    Help
+                  </a>
+                </p>
+              </div>
+
+              <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+                <p>
+                  <i class="fas fa-home me-2"></i> New York, NY 10012, US
+                </p>
+                <p>
+                  <i class="fas fa-envelope me-2"></i> info@example.com
+                </p>
+                <p>
+                  <i class="fas fa-phone me-2"></i> + 01 234 567 88
+                </p>
+                <p>
+                  <i class="fas fa-print me-2"></i> + 01 234 567 89
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <div class="container pt-4">
+          <section class="mb-4">
+            <a
+              class="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+              data-mdb-ripple-color="dark"
+            >
+              <i class="fab fa-facebook-f"></i>
+            </a>
+
+            <a
+              class="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+              data-mdb-ripple-color="dark"
+            >
+              <i class="fab fa-twitter"></i>
+            </a>
+
+            <a
+              class="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+              data-mdb-ripple-color="dark"
+            >
+              <i class="fab fa-google"></i>
+            </a>
+
+            <a
+              class="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+              data-mdb-ripple-color="dark"
+            >
+              <i class="fab fa-instagram"></i>
+            </a>
+
+            <a
+              class="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+              data-mdb-ripple-color="dark"
+            >
+              <i class="fab fa-linkedin"></i>
+            </a>
+
+            <a
+              class="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="#!"
+              role="button"
+              data-mdb-ripple-color="dark"
+            >
+              <i class="fab fa-github"></i>
+            </a>
+          </section>
+        </div>
+        <div class="text-center p-4">
+          © 2021 Copyright:
+          <a class="text-reset fw-bold" href="https://mdbootstrap.com/">
+            MDBootstrap.com
+          </a>
+        </div>
+      </footer>
     </div>
   );
 };
